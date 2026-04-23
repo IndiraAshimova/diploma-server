614242d40c24d6df4af5ef4d2550f5865486a946
IndiraAshimova <indira20041104@gmail.com>
2026-04-23 16:47:54 +0500

    auto migration on startup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-const startServer = async () => {await migrate();
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
+const migrate = require("../migrate");
 
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const xpRoutes = require("./routes/xpRoutes");
-const streakRoutes = require("./routes/streakRoutes"); 
+const streakRoutes = require("./routes/streakRoutes");
 
 const config = require("./config/env");
 
@@ -17,12 +20,18 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/xp", xpRoutes);
-app.use("/api/streak", streakRoutes); 
+app.use("/api/streak", streakRoutes);
 
 app.get("/healthz", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(config.port, () => {
-  console.log("Server running on port " + config.port);
-});
+const startServer = async () => {
+  await migrate();
+
+  app.listen(config.port, () => {
+    console.log("Server running on port " + config.port);
+  });
+};
+
+startServer();
