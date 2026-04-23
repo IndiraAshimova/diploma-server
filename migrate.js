24a24000ed87825d19d246e7124d328b86400d21
IndiraAshimova <indira20041104@gmail.com>
2026-04-23 16:34:29 +0500

    auto migration on startup

--- migrate.js
+++ migrate.js
@@ -3,25 +3,31 @@ const pool = require("./src/config/db");
 
 const migrate = async () => {
   try {
-    await pool.query(`ALTER TABLE users ADD COLUMN IF NOT EXISTS is_verified BOOLEAN DEFAULT FALSE;`);
-    console.log("is_verified добавлен");
+    await pool.query(`
+      ALTER TABLE users 
+      ADD COLUMN IF NOT EXISTS is_verified BOOLEAN DEFAULT FALSE;
+    `);
 
-    await pool.query(`ALTER TABLE users ADD COLUMN IF NOT EXISTS verification_token TEXT;`);
-    console.log("verification_token добавлен");
+    await pool.query(`
+      ALTER TABLE users 
+      ADD COLUMN IF NOT EXISTS verification_token TEXT;
+    `);
 
-    await pool.query(`ALTER TABLE users ADD COLUMN IF NOT EXISTS reset_token TEXT;`);
-    console.log("reset_token добавлен");
+    await pool.query(`
+      ALTER TABLE users 
+      ADD COLUMN IF NOT EXISTS reset_token TEXT;
+    `);
 
-    await pool.query(`ALTER TABLE users ADD COLUMN IF NOT EXISTS reset_token_expires TIMESTAMPTZ;`);
-    console.log("reset_token_expires добавлен");
+    await pool.query(`
+      ALTER TABLE users 
+      ADD COLUMN IF NOT EXISTS reset_token_expires TIMESTAMPTZ;
+    `);
 
-    console.log("Миграция завершена успешно!");
-    process.exit(0);
+    console.log("Миграция завершена!");
+
   } catch (err) {
     console.error("Ошибка миграции:", err.message);
-    process.exit(1);
   }
 };
 
 migrate();
-
