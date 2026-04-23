662ee71a98cbe4368ecf1fc444c7d5fbda24fbb0
IndiraAshimova <indira20041104@gmail.com>
2026-04-23 17:35:26 +0500

    test

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,14 @@ const authController = require("../controllers/authController");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/verify-email", authController.verifyEmail);      // GET Ч переход по ссылке из письма
+router.get("/test-email", async (req, res) => {
+  await emailService.sendVerificationEmail(
+    "indira20041104@gmail.com",
+    "test-token"
+  );
+
+  res.send("sent");
+});
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
 
